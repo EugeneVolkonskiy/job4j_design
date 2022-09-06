83ed60bfbce90436340614ff07662b4af9dc1572
EugeneVolkonskiy <3376361@mail.ru>
2022-09-06 14:10:32 +0300

    Утверждения с исключениями [#504886]

---
+++ src/test/java/ru/job4j/assertj/NameLoadTest.java
@@ -0,0 +1,50 @@
+package ru.job4j.assertj;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class NameLoadTest {
+    @Test
+    void checkEmpty() {
+        NameLoad nameLoad = new NameLoad();
+        assertThatThrownBy(nameLoad::getMap)
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessageContaining("no data");
+    }
+
+    @Test
+    void checkEmptyNames(){
+        NameLoad nameLoad = new NameLoad();
+        String[] names = new String[0];
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void whenNotContainsEquals(){
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"1 = A", "2B"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("2B");
+    }
+
+    @Test
+    void whenStartsWithEquals(){
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"1 = A", "=2B"};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("=2B");
+    }
+
+    @Test
+    void whenOnlyEquals(){
+        NameLoad nameLoad = new NameLoad();
+        String[] names = {"1 = A", "="};
+        assertThatThrownBy(() -> nameLoad.parse(names))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("=");
+    }
+}
