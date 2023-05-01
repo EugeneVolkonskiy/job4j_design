a39b7029863edd2abab5b16fddb24dd32c8f0e7f
EugeneVolkonskiy <3376361@mail.ru>
2023-05-01 22:28:32 +0300

    5.2. BufferedOutputStream

--- src/main/java/ru/job4j/io/ResultFile.java
+++ src/main/java/ru/job4j/io/ResultFile.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
+import java.io.BufferedOutputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 public class ResultFile {
 
@@ -14,6 +16,18 @@ public class ResultFile {
                 out.write(System.lineSeparator().getBytes());
             }
         } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (PrintWriter out = new PrintWriter(
+                new BufferedOutputStream(
+                        new FileOutputStream("data/result.txt")
+                ))) {
+            out.println("Hello, world!");
+            out.printf("%s%n", "Some string");
+            out.printf("%d%n", 10);
+            out.printf("%f%n", 1.5f);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
