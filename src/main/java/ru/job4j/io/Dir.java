7c5e577cb8a35e5bc20bb33652fdad84718d7ef1
EugeneVolkonskiy <3376361@mail.ru>
2023-05-04 19:15:01 +0300

    9.0. Файлы и директории. Класс File [#252491]

--- src/main/java/ru/job4j/io/Dir.java
+++ src/main/java/ru/job4j/io/Dir.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Dir {
     public static void main(String[] args) {
-        File file = new File("c:\\projects\\job4j_design");
+        File file = new File("c:\\projects");
         if (!file.exists()) {
             throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
         }
