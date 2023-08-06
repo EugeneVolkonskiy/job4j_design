b9fef7ee4ab55cd2e32bd9977ecdf8b467c41e91
EugeneVolkonskiy <3376361@mail.ru>
2023-08-06 17:41:13 +0300

    1. Реализация кеша на SoftReference [#1592 # [#1592]]

---
+++ src/main/java/ru/job4j/cache/DirFileCache.java
@@ -0,0 +1,27 @@
+package ru.job4j.cache;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class DirFileCache extends AbstractCache<String, String> {
+
+    private final String cachingDir;
+
+    public DirFileCache(String cachingDir) {
+        this.cachingDir = cachingDir;
+    }
+
+    @Override
+    protected String load(String key)  {
+        Path path = Paths.get(String.format("%s%s", cachingDir, key));
+        String value = "";
+        try {
+            value = Files.readString(path);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return value;
+    }
+}
---
+++ src/main/java/ru/job4j/cache/menu/Emulator.java
@@ -0,0 +1,12 @@
+package ru.job4j.cache.menu;
+
+import ru.job4j.cache.AbstractCache;
+import ru.job4j.cache.DirFileCache;
+
+public class Emulator {
+
+    public static void main(String[] args) {
+        AbstractCache<String, String> abstractCache = new DirFileCache("src/main/java/ru/job4j/cache/files/");
+        System.out.println(abstractCache.get("surnames.txt"));
+    }
+}
